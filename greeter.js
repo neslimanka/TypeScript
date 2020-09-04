65a10b0f09fb6f60191d26828e5da2fcdcb200a9
neslimanka <nesli.11.67@gmail.com>
2020-09-04 17:48:43 +0300

    .

---
+++ greeter.js
@@ -0,0 +1,7 @@
+function greeter(name) {
+    console.log('hello' + name);
+}
+var user = 'Neslihan';
+greeter(user);
+//typescriptın en guzel tarafı typescrip dosyasının içerisine javascript yazmış bile olsak 
+//gecerli bir typescrippt olmuş oluyor.
---
+++ greeter.ts
@@ -0,0 +1,8 @@
+function greeter(name) {
+console.log('hello ' + name);
+
+}
+let user = 'Neslihan'; // let ES6 ile gelen bir keyword ,her tarayıcı anlamaz.
+
+greeter(user); // .ts dosyasını .js e cevirmek için terminalden tsc greeter.ts komutunu yazdım.
+                //içerikte değişen şeyler yalnızca deişken tipleriydi.
